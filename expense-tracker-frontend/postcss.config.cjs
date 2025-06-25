bff186915f207f7cc2ff36716ce1b4d0bd39f1c3
george <ezegeorgechukwuma@gmail.com>
2025-06-25 09:52:14 +0100

    initializing react and tailwindcss

---
+++ expense-tracker-frontend/postcss.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
+}
+
+
+// const { registerUser, loginUser, getUserProfile } = require('../controllers/userController');
+// const protect = require('../middleware/authMiddleware');  
---
+++ expense-tracker-frontend/tailwind.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    content: ["./index.html", "./src/**/*.{js,jsx}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+// Compare this snippet from expense_tracker_backend/routes/userRoutes.js:
+// const express = require('express');  
