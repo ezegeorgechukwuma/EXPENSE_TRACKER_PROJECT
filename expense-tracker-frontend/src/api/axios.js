0f8b3e0f1b8733154bc93ed6c0d43c8f9484d66c
george <ezegeorgechukwuma@gmail.com>
2025-07-17 08:34:33 +0100

    Add CI/CD workflow for backend & frontend

--- expense-tracker-frontend/src/api/axios.js
+++ expense-tracker-frontend/src/api/axios.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api', // ✅ Update if hosted
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
 export default instance;
-// This Axios instance is configured to communicate with the backend API.
-// It sets the base URL and default headers for all requests.
